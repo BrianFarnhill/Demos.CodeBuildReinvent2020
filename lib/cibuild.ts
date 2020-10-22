c179acce5035832ba39274213fdb87823f122de4
Brian Farnhill <brfarn@amazon.com>
2020-10-22 17:14:09 +1100

    Added slack notification, and changed workflow to filter by branch

--- lib/cibuild.ts
+++ lib/cibuild.ts
@@ -2,30 +2,64 @@ import * as cdk from '@aws-cdk/core';
 import * as codebuild from '@aws-cdk/aws-codebuild';
 import * as stepfunctions from '@aws-cdk/aws-stepfunctions';
 import * as tasks from '@aws-cdk/aws-stepfunctions-tasks';
+import * as iam from '@aws-cdk/aws-iam';
+
+interface RawBuildTask {
+    Parameters: {
+        [key: string]: string
+    }
+}
 
 export default function(construct: cdk.Construct, project: codebuild.Project) {
 
-    const buildTask = new tasks.CodeBuildStartBuild(construct, "RunBuild", {
+    const endTask = new stepfunctions.Pass(construct, "Skip Build");
+
+    const buildTask = new tasks.CodeBuildStartBuild(construct, "RunBuildTemplate", {
         project,
         resultPath: "$.BuildResult",
         integrationPattern: stepfunctions.IntegrationPattern.RUN_JOB
-    })
+    });
+    
 
-    const endState = new stepfunctions.Succeed(construct, "Finished");
+    const rawBuildTask = buildTask.toStateJson() as RawBuildTask;
+    rawBuildTask.Parameters["SourceVersion.$"] = "States.Format('{}^\\{{}\\}', $.detail.referenceFullName, $.detail.commitId)";
+    
+    const buildTaskWithSource = new stepfunctions.CustomState(construct, "CI Build", {
+        stateJson: rawBuildTask
+    });    
 
-    const notifyOfFailure = new stepfunctions.Wait(construct, "DemoWait", {
-        time: stepfunctions.WaitTime.duration(cdk.Duration.seconds(5))
-    })
+    const checkBranch = new stepfunctions.Choice(construct, "CheckBranch", {})
+        .when(stepfunctions.Condition.stringMatches('$.detail.referenceName', 'feature-*'), buildTaskWithSource)
+        .otherwise(endTask);
 
-    const checkBuildSuccess = new stepfunctions.Choice(construct, "CheckBuildSuccess", {
-        inputPath: "$.BuildResult.Build.BuildStatus"
-    })
-        .when(stepfunctions.Condition.stringEquals('$', 'SUCCEEDED'), endState)
-        .otherwise(notifyOfFailure.next(endState));
+    const definition = checkBranch;
 
-    const definition = buildTask.next(checkBuildSuccess);
-
-    new stepfunctions.StateMachine(construct, "CiBuild", {
+    const sm = new stepfunctions.StateMachine(construct, "CiBuild", {
         definition
     });
+
+    sm.addToRolePolicy(new iam.PolicyStatement({
+        actions: [
+            "codebuild:BatchGetBuilds",
+            "codebuild:BatchGetReports",
+            "codebuild:StartBuild",
+            "codebuild:StopBuild",
+        ],
+        resources: [
+            project.projectArn
+        ]
+    }))
+
+    sm.addToRolePolicy(new iam.PolicyStatement({
+        actions: [
+            "events:DescribeRule",
+            "events:PutRule",
+            "events:PutTargets",
+        ],
+        resources: [
+            `arn:${cdk.Aws.PARTITION}:events:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:rule/StepFunctionsGetEventForCodeBuildStartBuildRule`
+        ]
+    }))
+
+    return sm;
 }
--- lib/demos.code_build_reinvent2020-stack.ts
+++ lib/demos.code_build_reinvent2020-stack.ts
@@ -2,11 +2,18 @@ import * as cdk from '@aws-cdk/core';
 import * as codebuild from '@aws-cdk/aws-codebuild';
 import * as codecommit from '@aws-cdk/aws-codecommit';
 import * as s3assets from '@aws-cdk/aws-s3-assets';
+import * as iam from '@aws-cdk/aws-iam';
+import * as targets from '@aws-cdk/aws-events-targets';
+import * as chatbot from '@aws-cdk/aws-chatbot';
+import * as notifications from '@aws-cdk/aws-codestarnotifications';
+import dotenv = require("dotenv");
 import ciBuild from './cibuild';
 
 export class DemosCodeBuildReinvent2020Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    dotenv.config();
 
     const demoCode = new s3assets.Asset(this, "DemoCode", {
       path: 'demorepo'
@@ -28,6 +35,42 @@ export class DemosCodeBuildReinvent2020Stack extends cdk.Stack {
       source: codebuild.Source.codeCommit({ repository: repo })
     });
 
-    ciBuild(this, buildProject);
+    buildProject.addToRolePolicy(new iam.PolicyStatement({
+      actions: [
+        "ssmmessages:CreateControlChannel",
+        "ssmmessages:CreateDataChannel",
+        "ssmmessages:OpenControlChannel",
+        "ssmmessages:OpenDataChannel"
+      ],
+      effect: iam.Effect.ALLOW,
+      resources: [ "*" ]
+    }));
+
+    const sm = ciBuild(this, buildProject);
+
+    repo.onCommit("OnCommit", {
+      target: new targets.SfnStateMachine(sm)
+    });
+
+    if (!process.env.SLACK_WORKSPACE_ID) throw new Error("Missing environment variable SLACK_WORKSPACE_ID");
+    if (!process.env.SLACK_CHANNEL_ID) throw new Error("Missing environment variable SLACK_CHANNEL_ID");
+
+    const chatChannel = new chatbot.SlackChannelConfiguration(this, "SlackNotifications", {
+      slackChannelConfigurationName: 'DemoBuildNotifications',
+      slackChannelId: process.env.SLACK_CHANNEL_ID,
+      slackWorkspaceId: process.env.SLACK_WORKSPACE_ID
+    });
+    
+
+    new notifications.CfnNotificationRule(this, "BuildFailNotifications", {
+      name: "FailedDemoBuilds",
+      resource: buildProject.projectArn,
+      detailType: 'FULL',
+      eventTypeIds: ['codebuild-project-build-state-failed'],
+      targets: [{
+        targetAddress: chatChannel.slackChannelConfigurationArn,
+        targetType: 'AWSChatbotSlack'
+      }],
+    });
   }
 }
